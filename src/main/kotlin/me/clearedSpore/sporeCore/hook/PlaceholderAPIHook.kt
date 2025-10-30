db5909298ebe3af59f14efd92fbfcc4f44679187
Clearedspore <spore1325@gmail.com>
2025-10-30 14:01:12 +0100

    feat: Added PlaceholderAPI support

--- src/main/kotlin/me/clearedSpore/sporeCore/hook/PlaceholderAPIHook.kt
+++ src/main/kotlin/me/clearedSpore/sporeCore/hook/PlaceholderAPIHook.kt
@@ -1,5 +1,7 @@
 package me.clearedSpore.sporeCore.hook
 
+import me.clearedSpore.sporeAPI.util.TimeUtil
+import me.clearedSpore.sporeCore.SporeCore
 import me.clearedSpore.sporeCore.features.eco.EconomyService
 import me.clearedSpore.sporeCore.features.eco.`object`.BalanceFormat
 import me.clearedSpore.sporeCore.user.UserManager
@@ -18,17 +20,29 @@ class PlaceholderAPIHook() : PlaceholderExpansion() {
     override fun onPlaceholderRequest(player: Player, params: String): String? {
         val user = UserManager.get(player) ?: return null
 
-        return when (params.lowercase()) {
-            "balance_raw" -> return user.balance.toString()
+        val args = params.lowercase().split("_")
 
-            "balance_formatted" -> {
-                val balance = EconomyService.format(user.balance, BalanceFormat.COMPACT).toString()
-                return balance
-            }
+        return when {
+            // %sporecore_balance_raw%
+            params.equals("balance_raw", ignoreCase = true) -> user.balance.toString()
 
-            "balance_decimal" -> {
-                val balance = EconomyService.format(user.balance, BalanceFormat.DECIMAL).toString()
-                return balance
+            // %sporecore_balance_formatted%
+            params.equals("balance_formatted", ignoreCase = true) ->
+                EconomyService.format(user.balance, BalanceFormat.COMPACT).toString()
+
+            // %sporecore_balance_decimal%
+            params.equals("balance_decimal", ignoreCase = true) ->
+                EconomyService.format(user.balance, BalanceFormat.DECIMAL).toString()
+
+            // %sporecore_homes%
+            params.equals("homes", ignoreCase = true) ->
+                SporeCore.instance.homeService.getAllHomes(user).size.toString()
+
+            // %sporecore_kit_timeleft_<kitName>%
+            args.size >= 3 && args[0] == "kit" && args[1] == "timeleft" -> {
+                val kitName = args.drop(2).joinToString("_")
+                val remaining = user.getKitCooldownRemaining(kitName)
+                if (remaining <= 0) "Ready" else TimeUtil.formatDuration(remaining)
             }
 
             else -> null
