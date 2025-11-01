1562299c86c613a5e2d3282f504650d16cb5dfa9
Clearedspore <spore1325@gmail.com>
2025-11-01 16:30:04 +0100

    feat: Kit update

---
+++ src/main/kotlin/me/clearedSpore/sporeCore/listener/DeathListener.kt
@@ -0,0 +1,42 @@
+package me.clearedSpore.sporeCore.listener
+
+import me.clearedSpore.sporeAPI.util.Logger
+import me.clearedSpore.sporeAPI.util.StringUtil.firstPart
+import me.clearedSpore.sporeAPI.util.StringUtil.hasFlag
+import me.clearedSpore.sporeCore.KitsConfig
+import me.clearedSpore.sporeCore.SporeCore
+import me.clearedSpore.sporeCore.util.InventoryUtil
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+
+
+class DeathListener : Listener {
+
+    @EventHandler
+    fun onDeath(event: PlayerDeathEvent) {
+        val player = event.player
+        val config = SporeCore.instance.coreConfig
+        val kitService = SporeCore.instance.kitService
+
+        if (config.kits.deathKit.isNotEmpty()) {
+            val kitName = config.kits.deathKit.firstPart()
+            val shouldClear = config.kits.deathKit.hasFlag("clear")
+            val kits = kitService.getAllKits()
+
+            val kit = kits.find { it.name.equals(kitName, ignoreCase = true) }
+
+            if(kit == null){
+                Logger.error("Failed to give death kit to ${player.name}")
+                return
+            }
+
+            if (shouldClear) {
+                player.inventory.clear()
+            }
+
+            kitService.giveKit(player, kitName)
+        }
+    }
+
+}
