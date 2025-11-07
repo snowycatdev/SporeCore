6c461070aef523b68a6609bb23de8504aa34f126
Clearedspore <spore1325@gmail.com>
2025-11-07 18:00:40 +0100

    feat: Add /sudo

---
+++ src/main/kotlin/me/clearedSpore/sporeCore/commands/SudoCommand.kt
@@ -0,0 +1,62 @@
+package me.clearedSpore.sporeCore.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Default
+import co.aikar.commands.annotation.Syntax
+import me.clearedSpore.sporeAPI.util.CC.blue
+import me.clearedSpore.sporeCore.SporeCore
+import me.clearedSpore.sporeCore.extension.PlayerExtension.userJoinFail
+import me.clearedSpore.sporeCore.util.Perm
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("sudo")
+@CommandPermission(Perm.SUDO)
+class SudoCommand : BaseCommand() {
+
+    @Default()
+    @Syntax("<player|*> <command|message(chat:)>")
+    @CommandCompletion("@players|*")
+    fun onSudo(sender: CommandSender, targetName: String, action: String) {
+
+        val targets: List<Player> = if (targetName == "*") {
+            Bukkit.getOnlinePlayers().toList()
+        } else {
+            val target = Bukkit.getPlayerExact(targetName)
+            if (target == null) {
+                sender.userJoinFail()
+                return
+            }
+            listOf(target)
+        }
+
+
+        targets.forEach { target ->
+            if (action.startsWith("chat:")) {
+                val message = action.removePrefix("chat:")
+                target.chat(message)
+            } else {
+                target.performCommand(action)
+            }
+        }
+
+        if (targets.size == 1) {
+            val t = targets[0]
+            if (action.startsWith("chat:")) {
+                sender.sendMessage("Successfully forced ${t.name} to send a chat message".blue())
+            } else {
+                sender.sendMessage("Successfully forced ${t.name} to run a command".blue())
+            }
+        } else {
+            if (action.startsWith("chat:")) {
+                sender.sendMessage("Successfully forced ${targets.size} players to send a chat message".blue())
+            } else {
+                sender.sendMessage("Successfully forced ${targets.size} players to run a command".blue())
+            }
+        }
+    }
+}
