170edc32d476784456aa122b827b5f75abc68c2e
xwinterkitty <141590124+snowycatdev@users.noreply.github.com>
2026-01-25 13:01:13 -1000

    Added Death Messages to Discord

--- src/main/kotlin/me/clearedSpore/sporeCore/listener/DeathListener.kt
+++ src/main/kotlin/me/clearedSpore/sporeCore/listener/DeathListener.kt
@@ -1,16 +1,50 @@
 package me.clearedSpore.sporeCore.listener
 
+import me.clearedSpore.sporeAPI.exception.LoggedException
 import me.clearedSpore.sporeAPI.util.Logger
 import me.clearedSpore.sporeAPI.util.StringUtil.firstPart
 import me.clearedSpore.sporeAPI.util.StringUtil.hasFlag
+import me.clearedSpore.sporeAPI.util.Webhook
 import me.clearedSpore.sporeCore.SporeCore
 import me.clearedSpore.sporeCore.annotations.AutoListener
+import me.clearedSpore.sporeCore.features.discord.DiscordService
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerRespawnEvent
 
 @AutoListener
 class DeathListener : Listener {
+
+    @EventHandler
+    fun onDeath(event: PlayerDeathEvent) {
+        val player = event.player
+        val config = SporeCore.instance.coreConfig
+        val deathMessage = event.deathMessage
+
+        if (config.discord.chat.isNotEmpty()) {
+            val embed = Webhook.Embed()
+                .setColor(0x00FF00)
+                .setDescription("**${deathMessage}**")
+
+            val webhook = Webhook(config.discord.chat)
+                .setProfileURL(DiscordService.getAvatarURL(player.uniqueId))
+                .setUsername(player.name)
+                .addEmbed(embed)
+
+            try {
+                webhook.send()
+            } catch (ex: Exception) {
+                throw LoggedException(
+                    userMessage = "Failed to send message to Discord.",
+                    internalMessage = "Failed to send message to Discord",
+                    channel = LoggedException.Channel.GENERAL,
+                    developerOnly = false,
+                    cause = ex
+                ).also { it.log() }
+            }
+        }
+    }
 
     @EventHandler
     fun onRespawn(event: PlayerRespawnEvent) {
